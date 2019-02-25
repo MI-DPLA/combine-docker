0ec8826a28a334b630b3b22c858bc683da29ec75
Graham Hukill <ghukill@gmail.com>
2019-02-25 12:37:09 -0500

    build tweaks

--- combine/combine_db_prepare.sh
+++ combine/combine_db_prepare.sh
@@ -1,4 +1,6 @@
 mysql -h 10.5.0.4 -u root -pcombine < /tmp/combine.sql
 python /opt/combine/manage.py makemigrations
 python /opt/combine/manage.py migrate
+python /opt/combine/manage.py makemigrations core
+python /opt/combine/manage.py migrate core
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('combine'"," 'root@none.com'"," 'combine')" | python /opt/combine/manage.py shell
