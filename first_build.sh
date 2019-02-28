9adee7d6904d3a355c964eab78d40afc60bfeeea
Graham Hukill <ghukill@gmail.com>
2019-02-28 07:40:54 -0500

    firstbuild inits combine app submodule

--- first_build.sh
+++ first_build.sh
@@ -1,10 +1,13 @@
-
 # echo
 echo "Running Combine-Docker first build, this should be run only once!"
+
+# init Combine app submodule and copy localsettings
+git submodule init
+git submodule update
+cp ./combine/localsettings.py ./combine/combine/combine/localsettings.py
 
 # format Hadoop namenode
 docker-compose run hadoop-namenode /bin/bash -c "mkdir -p /hdfs/namenode && echo 'Y' && /opt/hadoop/bin/hdfs namenode -format"
 
 # Combine db migrations and superuser create
-# TODO: pass mysql root password with run command
 docker-compose run combine-django /bin/bash -c "bash /tmp/combine_db_prepare.sh"
