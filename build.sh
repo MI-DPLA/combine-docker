e05fe981e6771de3255cc2b45b732e325d7782dc
Esty Thomas <esty@umich.edu>
2019-11-08 17:21:28 -0500

    flailing around trying to get static files to work

--- build.sh
+++ build.sh
@@ -8,14 +8,24 @@ docker-compose down
 echo $COMBINE_BRANCH
 
 # init Combine app submodule and use localsettings docker template
-git submodule init
-git submodule update
+#git submodule init
+#git submodule update
 cd combine/combine
-git fetch
-git checkout $COMBINE_BRANCH
-git pull
+#git fetch
+#git checkout $COMBINE_BRANCH
+#git pull
 if [[ ! -f "./combine/localsettings.py" ]]; then
     cp ./combine/localsettings.py.docker ./combine/localsettings.py
+fi
+if [[ ! -d "./static/" ]]; then
+    mkdir -p static/js/
+    cp ./core/static/* static/
+fi
+mv ./core/static/[^j]*/*.js static/js/
+mv ./core/static/*.js static/js/
+if [[ ! -f "./static/js/livy-ui.js" ]]; then
+    git clone https://github.com/apache/incubator-livy ~/livy
+    cp ~/livy/server/src/main/resources/org/apache/livy/server/ui/static/js/*.js ./static/js/
 fi
 cd ../../
 
