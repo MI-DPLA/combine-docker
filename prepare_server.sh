f9a2b7f9186951bb47024330efe54f9590ecf3ed
Esty Thomas <esty@umich.edu>
2019-11-22 10:57:25 -0500

    Add script to install all the docker things

---
+++ prepare_server.sh
@@ -0,0 +1,5 @@
+apt install docker.io
+curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
