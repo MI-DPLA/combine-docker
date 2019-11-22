b8f1bff1ca3491fd750ad891b12b02ff1c3e4786
Esty Thomas <esty@umich.edu>
2019-11-22 13:38:22 -0500

    Gotta actually start docker, doncha know

--- prepare_server.sh
+++ prepare_server.sh
@@ -1,5 +1,9 @@
-apt install docker.io
+apt update
+apt install -y docker.io
 curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+systemctl enable docker
+systemctl start docker
+usermod -aG docker $(whoami)
