2405b48367d3e6ca2e4735850c7ad17559b8e6cf
Esty Thomas <esty@umich.edu>
2020-01-20 15:14:22 -0500

    Move mysql to port 3307 to make automated tests easier to run

--- combine/combine_db_prepare.sh
+++ combine/combine_db_prepare.sh
@@ -1,14 +1,14 @@
 # run combine migrations
 echo "waiting for MySQL container to be ready..."
-while ! mysqladmin ping -h"mysql" --silent; do
+while ! mysqladmin ping -h "mysql" --port=3307 -pcombine --silent; do
     echo "waiting..."
     sleep 1
 done
 echo "ready!"
-mysql -h mysql -u root -pcombine < /tmp/combine.sql
+mysql -h mysql --port=3307 -u root -pcombine < /tmp/combine.sql
 python /opt/combine/manage.py makemigrations
 python /opt/combine/manage.py migrate
 python /opt/combine/manage.py makemigrations core
 python /opt/combine/manage.py migrate core
 python /opt/combine/manage.py createsuperuser
-python /opt/combine/manage.py collectstatic
+python /opt/combine/manage.py collectstatic --noinput --clear
