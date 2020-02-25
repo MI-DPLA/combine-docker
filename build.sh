037f6377b72e597201270f467408f64c9abc2840
Esty Thomas <esty@umich.edu>
2020-02-25 15:15:19 -0500

    End of build.sh should call buildstatic.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ WORKDIR=$(pwd)
 # bring down Combine docker containers, if running
 docker-compose down
 touch $WORKDIR/nginx/error.log
+if [[ ! -f "$WORKDIR/nginx/nginx.conf" ]]; then
+  cp $WORKDIR/nginx/nginx.conf.template $WORKDIR/nginx/nginx.conf
+fi
 
 # init Combine app submodule and use localsettings docker template
 echo $COMBINE_BRANCH
@@ -36,17 +39,4 @@ docker-compose run hadoop-namenode /bin/bash -c "echo 'Y' | /opt/hadoop/bin/hdfs
 # Combine db migrations and superuser create
 docker-compose run combine-django /bin/bash -c "bash /tmp/combine_db_prepare.sh"
 
-cp -r $WORKDIR/combine/combine/core/static/* static/
-cp -r $WORKDIR/combine/combine/static/[^j]*/*.js static/js/
-if [[ ! -d "$WORKDIR/external-static/livy/" ]]; then
-  mkdir -p $WORKDIR/external-static/livy
-fi
-cd $WORKDIR/external-static/livy
-svn export --force https://github.com/apache/incubator-livy/tags/$LIVY_TAGGED_RELEASE/server/src/main/resources/org/apache/livy/server/ui/static/js/
-cp ./js/*.js $WORKDIR/combine/combine/static/js/
-if [[ ! -d "$WORKDIR/external-static/spark/" ]]; then
-  mkdir -p $WORKDIR/external-static/spark
-fi
-cd $WORKDIR/external-static/spark
-svn export --force https://github.com/apache/spark/tags/v$SPARK_VERSION/core/src/main/resources/org/apache/spark/ui/static
-cp ./static/*.js $WORKDIR/combine/combine/static/
+$WORKDIR/buildstatic.sh
