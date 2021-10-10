f7fe0cf124436c5a9d34f2139674e01846a0f7a9
fruviad <fruviad@yahoo.com>
2021-10-09 22:28:15 -0400

    Add logging to build.sh
    Each time build.sh is run, a log is created in $WORKDIR/buildlogs/ with the directory name incorporating the current date-time.  Within this session-specific log directory is a single log file to which is appended all data dumped during the course of the script.
    
    Convention will be that data echoed by the build script or other scripts written by Combine devs will be prepended with "### BUILDLOG:" and any other app's output would (obviously) not include this.
    
    Output to the terminal in which the build.sh script is run will be limited due to the output redirection to log files.  This version of the script also tells the user at invocation the shell command needed to track verbose output of the install in real time.
    
    Some output is coming from scripts run within containers, and is a bit trickier to deal with...specifically the combine/combine_db_prepare.sh script.

--- build.sh
+++ build.sh
@@ -1,26 +1,62 @@
 #!/bin/bash
 
-echo "Running Combine-Docker build script.  Note: this may take some time, anywhere from 5-20 minutes depending on your hardware."
+# get datetime when script is run; use this for logfile names
+RUNDATE=`date +%Y-%m-%d_%H-%m-%S`
+WORKDIR=$(pwd)
 
 # source .env file
 source ./.env
-WORKDIR=$(pwd)
+
+# we will log output in output files in subdirectories named according to date-time of build run
+if [ ! -d $WORKDIR/buildlogs ]
+then
+    mkdir $WORKDIR/buildlogs
+fi
+BUILDLOGDIR=$WORKDIR/buildlogs/build-log_$RUNDATE
+mkdir $BUILDLOGDIR
+BUILDLOG=$BUILDLOGDIR/build-log_$RUNDATE.log
+
+echo ""
+echo ""
+
+echo "####################################################################################################"
+echo "### BUILDLOG:  Running Combine-Docker build script. "
+echo "### BUILDLOG:  Note: this may take some time, depending on your hardware and remote server response"
+echo "### BUILDLOG"
+echo "### BUILDLOG:  Base install directory (WORKDIR) is:  $WORKDIR" | tee -a $BUILDLOG
+echo "### BUILDLOG:  " | tee -a $BUILDLOG
+echo "### BUILDLOG:  To see logs as they're created:  tail -f $BUILDLOG "
+echo "### BUILDLOG:"
+echo "### BUILDLOG:  hit ENTER to continue..."
+read ANSWER
+echo "### BUILDLOG:  Base install directory (WORKDIR) is:  $WORKDIR" | tee -a $BUILDLOG
+
 
 # bring down Combine docker containers, if running
+echo "### BUILDLOG:  Bringing down Docker containers (if they're running)"  | tee -a $BUILDLOG
 docker-compose down
+
+# basic setup for nginx
 touch $WORKDIR/nginx/error.log
 if [[ ! -f "$WORKDIR/nginx/nginx.conf" ]]; then
   cp $WORKDIR/nginx/nginx.conf.template $WORKDIR/nginx/nginx.conf
 fi
 
+echo "### BUILDLOG:  Current COMBINE_BRANCH is:  $COMBINE_BRANCH"
+
 # init Combine app submodule and use localsettings docker template
-echo $COMBINE_BRANCH
-git submodule init
-git submodule update
+echo "### BUILDLOG:     Initializing Combine app submodule" 2>&1 | tee -a $BUILDLOG
+echo "### BUILDLOG:     Working with Combine git branch:  $COMBINE_BRANCH" 2>&1 | tee -a $BUILDLOG
+
+git submodule init   2>&1 | tee -a $BUILDLOG
+git submodule update 2>&1 | tee -a $BUILDLOG
+
+echo "### BUILDLOG:  Fetching desired Combine git branch" 2>&1 | tee -a $BUILDLOG
 cd $WORKDIR/combine/combine
 git fetch
 git checkout $COMBINE_BRANCH
 git pull
+
 if [[ ! -f "./combine/localsettings.py" ]]; then
     cp ./combine/localsettings.py.docker ./combine/localsettings.py
 fi
@@ -32,14 +68,23 @@ fi
 cd $WORKDIR
 
 # build images
+echo 
+echo 
+echo "### BUILDLOG:  Removing existing Docker images" 2>&1 | tee -a $BUILDLOG
 docker volume rm combine-docker_combine_python_env combine-docker_hadoop_binaries combine-docker_spark_binaries combine-docker_livy_binaries combine-docker_combine_tmp combine-docker_combinelib
-docker-compose build
+docker-compose build  | tee -a $BUILDLOG
+
+echo  2>&1 | tee -a $BUILDLOG
 
 # format Hadoop namenode
-docker-compose run hadoop-namenode /bin/bash -c "mkdir -p /hdfs/namenode"
-docker-compose run hadoop-namenode /bin/bash -c "echo 'Y' | /opt/hadoop/bin/hdfs namenode -format"
+echo "### BUILDLOG:  formatting hadoop-namenode" 2>&1 | tee -a $BUILDLOG
+docker-compose run hadoop-namenode /bin/bash -c "mkdir -p /hdfs/namenode"  1>>$BUILDLOG 2>&1
+docker-compose run hadoop-namenode /bin/bash -c "echo 'Y' | /opt/hadoop/bin/hdfs namenode -format"  1>>$BUILDLOG 2>&1
+
 
 # Combine db migrations and superuser create
+echo "### BUILDLOG:  Combine db migrations and creation of superuser" 2>&1 | tee -a $BUILDLOG
 docker-compose run combine-django /bin/bash -c "bash /tmp/combine_db_prepare.sh"
 
+echo "### BUILDLOG:  Passing control to buildstatic.sh" 2>&1 | tee -a $BUILDLOG
 $WORKDIR/buildstatic.sh
