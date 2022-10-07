763262ec4367780d65954cfa96e3e8b8fc779376
Peter Kukla <fruviad@yahoo.com>
2022-10-07 20:47:43 +0000

    Add screen/debugging output

--- combine/combine_db_prepare.sh
+++ combine/combine_db_prepare.sh
@@ -11,12 +11,27 @@ while [ ! "`mysqladmin ping -h mysql --port=3307 -pcombine --silent`" ]; do
     sleep 1
 done
 echo "    MySQL container is ready!"  | tee -a $BUILDLOG
+
+echo "### BUILDLOG:  Creating the combine db in mysql" 2>&1 | tee -a $BUILDLOG
 mysql -h mysql --port=3307 -u root -pcombine < /tmp/combine.sql 2>&1 | tee -a $BUILDLOG
-python /opt/combine/manage.py makemigrations 2>&1 | tee -a $BUILDLOG
+
+
+echo "### BUILDLOG:  Running /opt/combine/manage.py makemigrations" 2>&1 | tee -a $BUILDLOG
+python /opt/combine/manage.py makemigrations 2>&1 | tee -a $BUILDLOG 
+
+echo "### BUILDLOG:  Running /opt/combine/manage.py migrate" 2>&1 | tee -a $BUILDLOG
 python /opt/combine/manage.py migrate 2>&1 | tee -a $BUILDLOG
+
+echo "### BUILDLOG:  Running /opt/combine/manage.py makemigrations core" 2>&1 | tee -a $BUILDLOG
 python /opt/combine/manage.py makemigrations core 2>&1 | tee -a $BUILDLOG
+
+echo "### BUILDLOG:  Running /opt/combine/manage.py migrate core" 2>&1 | tee -a $BUILDLOG
 python /opt/combine/manage.py migrate core 2>&1 | tee -a $BUILDLOG
+
+echo "### BUILDLOG:  Running /opt/combine/maanage.py createsuperuser" 2>&1 | tee -a $BUILDLOG
 python /opt/combine/manage.py createsuperuser 2>&1 | tee -a $BUILDLOG
+
+echo "### BUILDLOG:  Running /opt/combine/maanage.py collectstatic" 2>&1 | tee -a $BUILDLOG
 python /opt/combine/manage.py collectstatic --noinput --clear 2>&1 | tee -a $BUILDLOG
 
 echo "### BUILDLOG:  /tmp/combine_db_prepare.sh script has finished" | tee -a $BUILDLOG
