cf8691faf322e9c4da19867158bcf66b7f9eff91
Peter Kukla <fruviad@yahoo.com>
2022-10-09 11:33:19 +0000

    Fix typos; document logic error to be addressed later

--- combine/combine_db_prepare.sh
+++ combine/combine_db_prepare.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+
+###
+### my logging logic is flawed in this file and needs to be revisited /pdk
+###
+
 echo "### BUILDLOG:  beginning the /tmp/combine_db_prepare.sh script" | tee -a $BUILDLOG
 
 BUILDLOG=$1
@@ -28,10 +33,10 @@ python /opt/combine/manage.py makemigrations core 2>&1 | tee -a $BUILDLOG
 echo "### BUILDLOG:  Running /opt/combine/manage.py migrate core" 2>&1 | tee -a $BUILDLOG
 python /opt/combine/manage.py migrate core 2>&1 | tee -a $BUILDLOG
 
-echo "### BUILDLOG:  Running /opt/combine/maanage.py createsuperuser" 2>&1 | tee -a $BUILDLOG
+echo "### BUILDLOG:  Running /opt/combine/manage.py createsuperuser" 2>&1 | tee -a $BUILDLOG
 python /opt/combine/manage.py createsuperuser 2>&1 | tee -a $BUILDLOG
 
-echo "### BUILDLOG:  Running /opt/combine/maanage.py collectstatic" 2>&1 | tee -a $BUILDLOG
+echo "### BUILDLOG:  Running /opt/combine/manage.py collectstatic" 2>&1 | tee -a $BUILDLOG
 python /opt/combine/manage.py collectstatic --noinput --clear 2>&1 | tee -a $BUILDLOG
 
 echo "### BUILDLOG:  /tmp/combine_db_prepare.sh script has finished" | tee -a $BUILDLOG
