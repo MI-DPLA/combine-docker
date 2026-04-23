84a09df5becedf0b8e3f46a96b1674a0a53d3fa9
Anthony Thomas <antmoth@umich.edu>
2026-04-23 12:35:05 -0400

    use password from env in db_prepare.sh

--- combine/combine_db_prepare.sh
+++ combine/combine_db_prepare.sh
@@ -2,12 +2,12 @@
 
 # run combine migrations
 echo "waiting for MySQL container to be ready..."
-while [ ! "`mysqladmin ping -h mysql --port=3307 -pcombine --silent`" ]; do
+while [ ! "`mysqladmin ping -h mysql --port=3307 -p$MYSQL_ROOT_PASSWORD --silent`" ]; do
     echo "waiting..."
     sleep 1
 done
 echo "ready!"
-mysql -h mysql --port=3307 -u combine -pcombine < /tmp/combine.sql
+mysql -h mysql --port=3307 -p$MYSQL_ROOT_PASSWORD < /tmp/combine.sql
 python /opt/combine/manage.py makemigrations
 python /opt/combine/manage.py migrate
 python /opt/combine/manage.py makemigrations core
